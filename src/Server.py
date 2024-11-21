5a0629ebda14ef51acd04d2bc59ba4b081ffdc11
unknown <nathanjsmith95@gmail.com>
2024-11-21 02:36:10 -0800

    removed fly.io files

--- src/Server.py
+++ src/Server.py
@@ -89,7 +89,7 @@ def post_message():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3000)
+    app.run(debug=True)
 
 # from flask import Flask, jsonify, request
 # from flask_cors import CORS
